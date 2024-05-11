f1fdfd4c47394e50940ddac8b04ccc9103627433
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-05-12 01:39:28 +0530

    arithmatics operation

--- datatype/conversation&operation.js
+++ datatype/conversation&operation.js
@@ -18,3 +18,46 @@ let convertToNumber = Number(newAge)
 //when we convert 0 to boolean value then we get the output that is "False"**
 //when we convert (emty String"") to boolean value then we get the output that is "False"**
 //when we convert (STring with value "ashamin") to boolean value then we get the output that is "true"**
+
+
+
+                                     
+
+/* LEARN ABOUT OPERATIONS */
+
+// 1.Arithmetics operations 
+
+// let value = 3
+// let negvalue = -value;
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%5);
+
+
+
+
+//2. Concatinate
+
+let str1 = "hello"
+let str2 = " ASHAMIN"
+
+str1 = str1 + str2;
+console.log(str1);
+
+
+// console.log(1+"2");
+// console.log("2" + 1);
+// console.log("2" + 1 + 1);
+// console.log(2 + 1 +"2");
+
+// console.log(+true);
+
+let counter = 100;
+counter++;
+console.log(counter)
+
+
