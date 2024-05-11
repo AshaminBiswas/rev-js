c2c2f6648b9ef4d36ff5cf6892988ad373173962
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-05-12 01:58:51 +0530

    This about Comparison

---
+++ datatype/Comparison.js
@@ -0,0 +1,11 @@
+// Comparison 
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log( null > 0);
+console.log( null == 0);
+console.log( null >= 0);
+
+//Avoid this type of comparison
