b828635265009190ed5d14f3c6b1a604e9a20715
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-05-13 02:14:39 +0530

    there are learn about memory how to use in javaScript

---
+++ Stack_And_Heep_in_Memory/Stack.js
@@ -0,0 +1,11 @@
+/*
+Memory
+there are two type of Memory 
+i.Stack (it use in primitive data type & its return a copy of the data )
+Ex: let myName = "Ashamin";
+let newName = myName;
+
+ii. Heep (it use in Non primitive data type & its return the original copy of the data type )
+
+
+ */
