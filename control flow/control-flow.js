03fbc0777cc91183540f596dc5d04a43e1082aeb
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-05-21 02:06:58 +0530

    this file about control flow

---
+++ control flow/control-flow.js
@@ -0,0 +1,44 @@
+// if statement
+// const isUserLoggedin = true;
+// const temperature = 40;
+// if(temperature < 50){
+//     console.log("less thsn 50");
+// }else{
+//     console.log("temperature is grater than 50")
+// };
+
+
+// const balance = 1000;
+// if (balance > 500) console.log("test");
+
+
+/// +++++++++++ Nesting ++++++++++\
+
+// const balance = 2000;
+
+// if (balance < 500) {
+//     console.log("less than 500");
+
+// }else if(balance < 1000){
+//     console.log("less than 1000");
+
+// }else if (balance < 1500){
+//     console.log("less than 1500");
+
+// }else{
+//     console.log("equal to 2000");
+// }
+
+
+const isUserLoggedin = true;
+const debiteCard = true;
+const loggedinFromGoogle = false;
+const loggedinFromEmail = true;
+
+if (isUserLoggedin && debiteCard && 2==3) {
+    console.log("You can buy");
+}
+
+// if(loggedinFromEmail || loggedinFromGoogle){
+//     console.log("user logged in");
+// }
