f5008007f61a2673429acbab4bbb758138abcb74
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-05-21 02:39:08 +0530

    switch case

---
+++ control flow/switch.js
@@ -0,0 +1,33 @@
+
+// Switch Case
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("the Month is January");
+        break;
+    case 2:
+        console.log("the Month is February");
+        break;
+    case 3:
+        console.log("the Month is March");
+        break;
+    case 4:
+        console.log("the Month is April");
+        break;
+
+    default:console.log("default is match");
+        break;
+}// op = the Month is March
+
