e6693be6f5f076fc44e89100af21d1e595d069aa
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-05-23 04:59:55 +0530

    this file about while and Dowhile loop

---
+++ loops/while&doWhile.js
@@ -0,0 +1,19 @@
+let i = 0;
+while (i <= 10) {
+    // console.log(`value is ${i}`);
+    i = i + 2;
+}
+
+
+const myArray = ["virat", "rohit", "surya", "shiraj", "bhoom"]
+let arr = 0;
+while (arr<myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr++;
+}
+
+let score = 0
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score < 10);
