4f38f19c4a000f9fceeca2939662f8ecbb59e627
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2024-06-07 17:41:25 +0530

    jkjhku

--- promicess/promises.js
+++ promicess/promises.js
@@ -108,13 +108,3 @@
 
 
 
-fetch("http://jsonplaceholder.typicode.com/users")
-.then((responce)=>{
-    return responce.json()
-})
-.then((data)=>{
-    console.log(data);
-})
-.catch((err)=>{
-    console.log(err);
-})
